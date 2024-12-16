28742259ca5d3d44b267b9c7d21026f11ea8dbc8
Martin Repvik Olsbø <martinolsbo1@gmail.com>
2024-12-16 05:26:52 -0800

    removed unsued files, moved running of application to app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-from gui import GUI
+import tkinter as tk
+from home_screen import show_home_screen
 
-gui = GUI()
+class app():
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('hvlChessRobot')
+        self.root.geometry('800x600')
+        show_home_screen(self.root)
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    app()
